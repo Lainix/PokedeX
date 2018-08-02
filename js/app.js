d0b77b6baa159b629b0fa5a2811fd0ee9a9bbdc5
Lainix <linkelin8@gmail.com>
2018-08-02 16:39:54 -0500

    Se modifica css para que sea responsivo, y en js se agrega funcion inicial y variables globales

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+$(function () {
+  var pokemonSearch;
+  var defaultPokemon = '1'
+  var defaultPokemonData;
+
+  var pokeApi = function () {
+    defaultPokemonData = $.ajax({
+      url: "https://pokeapi.co/api/v2/pokemon/" + defaultPokemon,
+      method: "GET",
+    });
+});
  
