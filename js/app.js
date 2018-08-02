fcfd87460e0434ef3c0a2fb5a5f076a761f21fce
Lainix <linkelin8@gmail.com>
2018-08-02 18:41:07 -0500

    Se implemente mejor diseño de css, y en js se completa funcion para traer imagen y nombre de el pokemon

--- js/app.js
+++ js/app.js
@@ -8,5 +8,20 @@ $(function () {
       url: "https://pokeapi.co/api/v2/pokemon/" + defaultPokemon,
       method: "GET",
     });
+    defaultPokemonData.done(function (data) {
+      defaultPokemonData = data;
+      $('.pokedex h3').text(defaultPokemonData.name.toUpperCase())
+      $('.poke-img img').attr('src', defaultPokemonData.sprites.front_default)
+      console.log(data)
+    });
+
+    defaultPokemonData.fail(function (jqXHR, textStatus, error) {
+      alert("Request failed: " + textStatus + ' ' + error);
+    });
+  }
+
+  pokeApi()
+
+  
 });
  
