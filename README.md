a3bdbcffc57eaf602628514a74889d858bb68d97
Lainix <linkelin8@gmail.com>
2018-08-03 10:36:02 -0500

    Funcion para el boton para que traiga la informacion de el pokemon, nombre de el pokemon, pes, numero, abilidad, base experiencia, tipo

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 >¿Alguna ves viste Pokemon? Sino, no importa, ahora te enterarás de que trata. Un pokedex es un buscador de Pokemons, donde puedes obtener información sobre el pokemon que buscas. Usando la API de Pokeapi diseña tu propio pokedex
 
 ***
+![Gif page](assets/img/pokedex.PNG)
--- js/app.js
+++ js/app.js
@@ -22,6 +22,29 @@ $(function () {
 
   pokeApi()
 
-  
+  $('.btn').on('click', function () {
+
+    pokemonSearch = $('.pokedex input[type="text"]').val()
+
+    var request = $.ajax({
+      url: "https://pokeapi.co/api/v2/pokemon/" + pokemonSearch,
+      method: "GET",
+    });
+
+    request.done(function (data) {
+      $('.pokedex h3').text(data.name.toUpperCase())
+      $('.poke-img img').attr('src', data.sprites.front_default)
+      $('.p').html("Peso: " + data.weight)
+      $('.p2').html("No. " + data.id)      
+      $('.p3').html("Abilidad: " + data.abilities["0"].ability.name)      
+      $('.p4').html("Base exp: " + data.base_experience)
+      $('.p5').html("Tipo: " + data.types["0"].type.name + " , " + data.types["1"].type.name)
+      console.log(data)
+    });
+
+    request.fail(function (jqXHR, textStatus, error) {
+      alert("Request failed: " + textStatus + ' ' + error);
+    });
+  })
 });
  
